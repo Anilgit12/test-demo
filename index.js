3befe9d9c1f6255150e30302506b5b7814e772f9
anilgp567 <anil.gp@accionlabs.com>
2022-08-08 12:07:52 +0530

    for revert purpose

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 let text="hello i am master branch"
+let text1="hello i am master branch"
+let text2="hello i am master branch"
+let text3="hello i am master branch"
+let text4="hello i am master branch"
